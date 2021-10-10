c7e5d9132c823025935be8de7423a789e02697a1
Tiago M Palte <tiagompalte@hotmail.com>
2021-10-10 12:39:49 -0300

    fix: correção ao setar o name nos errors

--- src/errors/invalid-id-error.ts
+++ src/errors/invalid-id-error.ts
@@ -3,5 +3,6 @@ import { RepositoryError } from './repository-error'
 export class InvalidIdError extends RepositoryError {
   constructor (message?: string, error?: any) {
     super(message || 'Invalid identifier', error)
+    this.name = 'InvalidIdError'
   }
 }
